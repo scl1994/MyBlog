6642c7eba4378a67522a355e13ac230b0591c59f
scl1994 <sunchenglu1994@163.com>
2017-05-23 11:45:04 +0800

    password
    add flask-mail package

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
 
-from . import mian
+from . import main
 from .forms import NameForm
 from .. import db
 from ..models import User
 
 
-@mian.route("/", methods=["GET", "POST"])
+@main.route("/", methods=["GET", "POST"])
 def index():
     form = NameForm()
     if form.validate_on_submit():
