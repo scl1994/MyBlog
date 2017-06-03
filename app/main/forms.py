dd586a219afd7e0a5efc89a86a7dfdbfe9177242
scl1994 <sunchenglu1994@163.com>
2017-06-03 17:56:08 +0800

    edit profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import Required, Length
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+
+# 用户资料表单
+class EditProfileForm(FlaskForm):
+    name = StringField("Real name", validators=[Length(0, 64)])
+    location = StringField("Location", validators=[Length(0, 64)])
+    about_me = TextAreaField("About me")
+    submit = SubmitField("Submit")
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, abort
+from flask import render_template, session, redirect, url_for, abort, flash
+from flask_login import current_user, login_required
 
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
 
@@ -25,3 +26,20 @@ def user(username):
     if user is None:
         abort(404)
     return render_template("user.html", user=user)
+
+
+@main.route("/edit-profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash("Your profile has been updated.")
+        return redirect(url_for(".user", username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", form=form)
