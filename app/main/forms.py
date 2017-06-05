5b34c0b6595496fa7a80ec9bb2a24727e548ed85
scl1994 <sunchenglu1994@163.com>
2017-06-05 16:57:22 +0800

    edit admin profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import Required, Length
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import Required, Length, Email, Regexp
+from ..models import Role, User
+from wtforms import ValidationError
 
 
 class NameForm(FlaskForm):
@@ -8,9 +10,40 @@ class NameForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-# 用户资料表单
+# 用户级别的用户资料表单
 class EditProfileForm(FlaskForm):
     name = StringField("Real name", validators=[Length(0, 64)])
     location = StringField("Location", validators=[Length(0, 64)])
     about_me = TextAreaField("About me")
     submit = SubmitField("Submit")
+
+
+# 管理员级别的用户资料编辑器
+class EditProfileAdminForm(FlaskForm):
+    email = StringField("Email", validators=[Required(), Length(1, 64), Email()])
+    username = StringField("Username",
+                           validators=[Required(), Length(1, 64),
+                                       Regexp(r"^[a-zA-Z][a-zA-Z0-9_.]*$", 0,
+                                              "Username must have only letters,numbers,dots or underscores.")])
+    confirmed = BooleanField("Confirmed")
+    role = SelectField("Role", coerce=int)
+    name = StringField("Real name", validators=[Length(1, 64)])
+    location = StringField("Location", validators=[Length(1, 64)])
+    about_me = TextAreaField("About me")
+    submit = SubmitField("Submit")
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name)
+                             for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+                User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if field.data != self.username.data and \
+                User.query.filter_by(username=field.data).first():
+            raise ValidationError("Username already in use.")
--- app/main/views.py
+++ app/main/views.py
@@ -3,9 +3,10 @@ from flask import render_template, session, redirect, url_for, abort, flash
 from flask_login import current_user, login_required
 
 from . import main
-from .forms import NameForm, EditProfileForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm
 from .. import db
-from ..models import User
+from ..models import User, Role
+from ..decorators import admin_required
 
 
 @main.route("/", methods=["GET", "POST"])
@@ -28,6 +29,7 @@ def user(username):
     return render_template("user.html", user=user)
 
 
+# 用户级别的资料编辑器
 @main.route("/edit-profile", methods=["GET", "POST"])
 @login_required
 def edit_profile():
@@ -43,3 +45,31 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template("edit_profile.html", form=form)
+
+
+# 管理员级别的资料编辑器
+@main.route("/edit-profile/<int:id>", methods=["GET", "POST"])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash("The profile has been updated")
+        return redirect(url_for(".user", username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template("edit_profile.html", form=form, user=user)
