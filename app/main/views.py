0ffd1c8a043ea1caad73fb9f6e01dc8c0b948a08
scl1994 <sunchenglu1994@163.com>
2017-06-08 22:28:01 +0800

    posts pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, abort, flash
+from flask import render_template, session, redirect, url_for, abort, flash, request, current_app
 from flask_login import current_user, login_required
 
 from . import main
@@ -18,8 +18,13 @@ def index():
         post = Post(body=form.body.data, author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for(".index"))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template("index.html", form=form, posts=posts)
+    # 分页显示博客文章
+    page = request.args.get("page", 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config["FLASKY_POSTS_PER_PAGE"],
+        error_out=False)
+    posts = pagination.items
+    return render_template("index.html", form=form, posts=posts, pagination=pagination)
 
 
 @main.route("/user/<username>")
