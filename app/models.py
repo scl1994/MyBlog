90c2ca508345adcbf0a4ed33eac3fa15261c4085
scl1994 <sunchenglu1994@163.com>
2017-06-09 10:21:02 +0800

    : Rich text server side handling with Markdown and Bleach

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from datetime import datetime
+from markdown import markdown
+import bleach
 
 
 class Permission:
@@ -208,6 +210,8 @@ class Post(db.Model):
     __tabename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    # 添加markdown解析后的html代码
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
@@ -226,6 +230,16 @@ class Post(db.Model):
                      author=u)
             db.session.add(p)
             db.session.commit()
+
+    # 处理markdown文本
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em',
+                        'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
+                                                       tags=allowed_tags, strip=True))
+# 监听body是否有改变，改变了就立即更新
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 class AnonymousUser(AnonymousUserMixin):
