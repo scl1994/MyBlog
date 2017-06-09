e5de8afb5118de4d4feeef2cfa0145d25084e60d
scl1994 <sunchenglu1994@163.com>
2017-06-09 23:09:16 +0800

    more followers

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,7 @@ from . import main
 from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
 from ..models import User, Role, Post, Permission
-from ..decorators import admin_required
+from ..decorators import admin_required, permission_required
 
 
 @main.route("/", methods=["GET", "POST"])
@@ -106,3 +106,66 @@ def edit(id):
         return redirect(url_for('.post', id=post.id))
     form.body.date = post.body
     return render_template("edit_post.html", form=form)
+
+
+# 关注路由和视图函数
+@main.route('/follow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("Invalid user!")
+        return redirect(url_for('.index'))
+    if current_user.is_following(user):
+        flash('You are already following this user.')
+        return redirect(url_for('.user', username=username))
+    current_user.follow(user)
+    flash('You are now following %s.' % username)
+    return redirect(url_for('.user', username=username))
+
+
+# 取消关注视图函数
+@main.route('/unfollow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user!')
+        return redirect(url_for('.index'))
+    if not current_user.is_following(user):
+        flash('You are not following this user.')
+        return redirect(url_for('.user', username=username))
+    current_user.unfollow(user)
+    flash("You are not following %s anymore." % username)
+    return redirect(url_for('.user', username=username))
+
+
+# 关注者视图函数
+@main.route('/followers/<username>')
+def followers(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("Invalid user!")
+        return redirect(url_for('.index'))
+    page = request.args.get('page', 1, type=int)
+    pagination = user.followers.paginate(
+        page, per_page=current_app.config['FLASKY_FOLLOWERS_PER_PAGE'], error_out=False)
+    follows = [{'user': item.follower, 'timestamp': item.timestamp} for item in pagination.items]
+    return render_template('followers.html', user=user, title='Followers of',
+                           endpoint='.followers', pagination=pagination, follows=follows)
+
+
+@main.route('/followed-by/<username>')
+def followed_by(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("Invalid user!")
+        return redirect(url_for('.index'))
+    page = request.args.get('page', 1, type=int)
+    pagination = user.followed.paginate(
+        page, per_page=current_app.config['FLASKY_FOLLOWERS_PER_PAGE'], error_out=False)
+    follows = [{'user': item.followed, 'timestamp': item.timestamp} for item in pagination.items]
+    return render_template('followers.html', user=user, title="Followed by",
+                           endpoint='.followed_by', pagination=pagination, follows=follows)
--- config.py
+++ config.py
@@ -15,7 +15,10 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN")
     FLASKY_MAIL_SENDER = "Flasky Admin<%s>" % FLASKY_ADMIN
+    # 每页显示的博客文章的数量
     FLASKY_POSTS_PER_PAGE = 20
+    # 每页显示的关注着的数量
+    FLASKY_FOLLOWERS_PER_PAGE = 50
 
     @staticmethod
     def init_app(app):
